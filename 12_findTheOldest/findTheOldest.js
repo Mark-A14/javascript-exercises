7612db81fe535f461372a5ac5dc9a41a4ee33b05
MarkA <marklacap14.ml@gmail.com>
2023-07-16 16:39:38 +0800

    Exercise Complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (persons) {
+	let newlist = persons.sort((a, b) => {
+		return (a.yearOfDeath || 2023) - a.yearOfBirth <
+			(b.yearOfDeath || 2023) - b.yearOfBirth
+			? 1
+			: -1;
+	});
+	return newlist[0].name;
 };
 
 // Do not edit below this line
