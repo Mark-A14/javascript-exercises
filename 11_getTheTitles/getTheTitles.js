426265f8e00d6fd7acab60f8e91fe2832b040f1c
MarkA <marklacap14.ml@gmail.com>
2023-07-16 16:08:22 +0800

    Exercise Complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(books) {
+    let result = [];
+    for(book of books){
+      result.push(book.title);
+    }
+    return result;
 
 // Do not edit below this line
 module.exports = getTheTitles;
