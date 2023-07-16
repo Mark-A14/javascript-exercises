ecf830d8b6fb37cbe98edc81c789c81e0cbae17b
MarkA <marklacap14.ml@gmail.com>
2023-07-16 15:39:46 +0800

    Exercise Complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+	let punctuations = /[\.,?!\s]/g;
+	string = string.replace(punctuations, "").toLowerCase();
+	let backwardIndex = string.length - 1;
+	for (let i = 0; i < string.length; i++) {
+		if (string[i] !== string[backwardIndex]) {
+			return false;
+		}
+		backwardIndex -= 1;
+	}
+	return true;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (fibPlace) {
+	fibPlace = parseInt(fibPlace, 10);
+	if (fibPlace == -1 || fibPlace < 1) {
+		return "OOPS";
+	}
+	let prevFib = 0;
+	let fibTotal = 0;
+	let nextFib = 1;
+	for (let i = 0; i < fibPlace; ++i) {
+		fibTotal = nextFib + prevFib;
+		prevFib = nextFib;
+		nextFib = fibTotal;
+	}
+	return prevFib;
 };
 
 // Do not edit below this line
